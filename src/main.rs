c05bfc8805ff115535f69685900db222274c7be4
b1018043 <grey.fox.0417@gmail.com>
2021-03-15 16:18:02 +0900

    make mount proc available

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::process::{Command};
 use std::fs::File;
 use std::io::{Error,Write};
+use std::env;
 
 extern crate nix;
 use nix::sched::{unshare,CloneFlags};
 use nix::unistd::{getuid,getgid,sethostname};
+use nix::mount::{mount,MsFlags};
 
 struct UidGidMap{
     container_id: u32,
@@ -13,6 +15,25 @@ struct UidGidMap{
 }
 
 fn main() {
+    
+    let args: Vec<String> = env::args().collect();
+    
+    match args.len(){
+        1 => println!("invalid num of args."),
+        2 => {
+            let subcmd = &args[1];
+            match &subcmd[..]{
+                "run" => run(),
+                "child" => child(),
+                _ => eprintln!("err: invalid arg")
+            }
+        }
+        _ => eprintln!("err: invalid arg len.")
+    };
+}
+
+fn run(){
+
     let uid_map = UidGidMap{
         container_id: 0,
         host_id: getuid().as_raw(),
@@ -23,12 +44,34 @@ fn main() {
         host_id: getgid().as_raw(),
         size: 1,
     };
-    unshare(CloneFlags::CLONE_NEWUSER|CloneFlags::CLONE_NEWUTS|CloneFlags::CLONE_NEWIPC).expect("Failed to unshare.");
+
+    unshare(
+        CloneFlags::CLONE_NEWUSER|
+        CloneFlags::CLONE_NEWUTS|
+        CloneFlags::CLONE_NEWIPC|
+        CloneFlags::CLONE_NEWNET|
+        CloneFlags::CLONE_NEWPID|
+        CloneFlags::CLONE_NEWNS
+    ).expect("Failed to unshare.");
+
     add_mapping("/proc/self/uid_map", &uid_map).expect("failed add uid");
     init_setgroups();
     add_mapping("/proc/self/gid_map", &gid_map).expect("failed add gid");
+
+    let mut p = Command::new("/proc/self/exe").arg("child").spawn().expect("sh command failed to start");
+    p.wait().expect("[Error]: failed to wait");
+}
+
+fn child(){
+    mount(
+        Some("proc"),"/proc",
+        Some("proc"),MsFlags::empty(),
+        None::<&str>
+    ).expect("failed to mount fs");
+
     sethostname("container").expect("failed to hostname");
-    let mut p = Command::new("sh").spawn().expect("sh command failed to start");
+
+    let mut p = Command::new("/bin/sh").spawn().expect("sh command failed to start");
     p.wait().expect("[Error]: failed to wait");
 }
 
